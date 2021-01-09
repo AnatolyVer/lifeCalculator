69e1ff53943ab14d86ea3a70b8b46bd2057aaf89
AnatolyVer <tolik.vereshchak88@gmail.com>
2021-01-09 11:35:26 +0200

    updatr

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict'
 
-const button1 = document.getElementById("1")
-const button2 = document.getElementById("2")
+const buttonEarn = document.getElementById("buttonEarn")
+const buttonLose = document.getElementById("buttonLose")
 const list = document.getElementById("list")
 const change = document.getElementById("changeConfirm")
 const closeM = document.getElementById("close")
+const payment = document.getElementById("payment")
 let idCh
 
-const getItem = ({product,price,id}) => 
+
+const checkLocStor = key => {
+  if (!localStorage.getItem(key)) {
+    localStorage.setItem(key, 0)
+    if(key === 'cart') localStorage.setItem(key,JSON.stringify([]))
+  }
+}
+
+const getItem = ({product,price}) => {
+const id = new DataCue().getData
 `<div class="product" id="product-${id}"><span class="listContent">  ${product}</span>
 <span class="contentPrice" style="color:red">-${price}</span>
 <button class="rButton" data-id="${id}">Редактировать</button>
 <button class="deleteButton" id="${id}">Удалить</button>
 </div>`
+}
 
 
 const calculateLost = () => {
@@ -24,15 +35,9 @@ const calculateLost = () => {
 
 
 const getData = key =>{
-  if (!localStorage.getItem('cart')) {
-    localStorage.setItem('cart', JSON.stringify([]));
-  }
-  if (!localStorage.getItem('earning')) {
-    localStorage.setItem('earning', 0);
-  }
-  if (!localStorage.getItem('losing')) {
-    localStorage.setItem('losing', 0);
-  }
+  checkLocStor('cart')
+  checkLocStor('earning')
+  checkLocStor('losing')
   return localStorage.getItem(key)
 }
 
@@ -85,8 +90,8 @@ const closeModal = () =>  modal.style.display = "none"
 
 
 const Refresh = () => {
-while(list.firstChild)  list.firstChild.remove()
-while(payment.firstChild)  payment.firstChild.remove()
+list.replaceChild()
+payment.replaceChild()
 payment.insertAdjacentHTML('beforeend', getStatus())
 const data = JSON.parse(getData('cart'));
 data.forEach((product) =>
@@ -96,7 +101,7 @@ data.forEach((product) =>
 
 const confirmEarn = ({ target }) => {
    const earn = document.getElementById("inputEarn").value
-   if (earn > 0 && earn < 100000000) localStorage.setItem("earning",Number(getData('earning')) + Number(earn))
+   localStorage.setItem("earning",Number(getData('earning')) + Number(earn))
    Refresh()
   }
 
@@ -105,14 +110,12 @@ const confirmLose = ({ target }) => {
     const product = document.getElementById("inputProduct").value
     const price = document.getElementById("inputPrice").value
     if (product && price){
-      if (price > 0 && price < 100000000){
-        localStorage.setItem("losing",Number(getData('losing')) + Number(price))
-        const arr = JSON.parse(getData('cart'));
-        let id 
-        arr[arr.length - 1]?.id ? id = arr[arr.length - 1].id + 1 : id = 1 //проверка  для установки id для продукта
-        arr.push({product,price,id})
-        localStorage.setItem('cart', JSON.stringify(arr));
-      }
+    localStorage.setItem("losing",Number(getData('losing')) + Number(price))
+    const arr = JSON.parse(getData('cart'));
+    let id 
+    arr[arr.length - 1]?.id ? id = arr[arr.length - 1].id + 1 : id = 1 //проверка  для установки id для продукта
+    arr.push({product,price,id})
+    localStorage.setItem('cart', JSON.stringify(arr));
     }
     Refresh()
     calculateLost()
@@ -121,8 +124,8 @@ const confirmLose = ({ target }) => {
 
 
 const init = () => {
-button1.addEventListener('click', confirmEarn)
-button2.addEventListener('click', confirmLose)
+buttonEarn.addEventListener('click', confirmEarn)
+buttonLose.addEventListener('click', confirmLose)
 list.addEventListener('click', deleteProduct)
 change.addEventListener('click', changeClick)
 closeM.addEventListener('click', closeModal)
